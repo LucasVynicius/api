c6981031fbaa96cbd8c6d479a8e0a2e1da69f7d4
Lucas Albuquerque <lucas.albuquerque@sudema.pb.gov.br>
2026-05-19 17:08:20 +0100

    Initial commit project study spring

---
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -0,0 +1,33 @@
+package com.eventostec.api.domain.event;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "event")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String title;
+
+    private String description;
+
+    private String eventURL;
+
+    private Boolean remote;
+
+    private Date date;
+}
