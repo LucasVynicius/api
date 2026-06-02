2fa096bfcf66d5b096c3b445d9c594a9d61161a6
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2026-06-02 23:38:26 +0100

    config: add AWS S3 client configuration

---
+++ src/main/java/com/eventostec/api/config/AWSConfig.java
@@ -0,0 +1,39 @@
+package com.eventostec.api.config;
+
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSConfig {
+
+    @Value("${aws.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aws.secret-access-key}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String awsRegion;
+
+    @Bean
+    public AmazonS3 createS3Instance(){
+
+        AWSCredentials credentials = new BasicAWSCredentials(
+                accessKeyId,
+                secretKey
+        );
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withRegion(awsRegion)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
+}
