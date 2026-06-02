b33ad268b1fc91d8be20f456c02ab3508f4027b8
Lucas Albuquerque <luucas.albuquerque23@gmail.com>
2026-06-02 23:38:26 +0100

    repository: add event filtering queries

--- src/main/java/com/eventostec/api/repositories/EventRepository.java
+++ src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -1,9 +1,36 @@
 package com.eventostec.api.repositories;
 
 import com.eventostec.api.domain.event.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    @Query("SELECT e FROM Event e LEFT JOIN e.address a WHERE e.date >= :currentDate")
+    public Page<Event> findUpcomingEvent(@Param("currentDate") Date currentDate, Pageable pageable);
+
+    @Query("""
+    SELECT e FROM Event e
+    LEFT JOIN e.address a
+    WHERE (:title IS NULL OR LOWER(e.title) LIKE LOWER(CONCAT('%', :title, '%')))
+      AND (:city IS NULL OR LOWER(a.city) LIKE LOWER(CONCAT('%', :city, '%')))
+      AND (:uf IS NULL OR LOWER(a.uf) LIKE LOWER(CONCAT('%', :uf, '%')))
+      AND e.date >= :startDate
+      AND e.date <= :endDate
+    """)
+    Page<Event> findFilteredEvents(@Param("title") String title,
+                                   @Param("city") String city,
+                                   @Param("uf") String uf,
+                                   @Param("startDate") Date startDate,
+                                   @Param("endDate") Date endDate,
+                                   Pageable pageable);
+
+
 }
